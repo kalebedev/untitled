78cedd6d8f77a0c314b70fe40af18b9d1d27c820
konstantin <k.a.lebedev07@gmail.com>
2021-05-09 01:19:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+    Typography,
+    AppBar,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    CssBaseline,
+    Grid,
+    Toolbar,
+    Container, Button
+} from '@material-ui/core';
+import {PhotoCamera} from '@material-ui/icons';
+import useStyles from "./styles";
+
+
+const App = () => {
+    const classes = useStyles();
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return (
+        <>
+            <CssBaseline/>
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera className={classes.icon}/>
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div className={classes.container}>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Hello everyone This is a photoalbum and I'm trying to make this sentence as long as possible
+                        </Typography>
+                        <div className={classes.button}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        Primary  action
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+
+                    </Container>
+                    <Container className={classes.cardGrid} maxWidth="md">
+                        <Grid container spacing={4}>
+                            {cards.map((card) => (
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card className={classes.cardGrid}>
+                                        <CardMedia
+                                            className={classes.cardMedia}
+                                            image="https://source.unsplash.com/random"
+                                            title="Image Title" />
+                                        <CardContent className={classes.cardContent}>
+                                            <Typography gutterBottom variant="h5">
+                                                Heading
+                                            </Typography>
+                                            <Typography>
+                                                This is a media card. You can use this section to describe the content
+                                            </Typography>
+
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small" color="primary">View</Button>
+                                            <Button size="small" color="primary">Edit</Button>
+                                        </CardActions>
+                                    </Card>
+
+                                </Grid>
+                            ))}
+
+                        </Grid>
+
+                    </Container>
+                </div>
+            </main>
+            <footer>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textPrimary">
+                    Footer purpose
+                </Typography>
+
+            </footer>
+
+        </>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactFom from "react-dom"
+
+import App from './App'
+
+ReactFom.render(<App />, document.getElementById('root'));
+
